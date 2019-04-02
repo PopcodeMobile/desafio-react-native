ca604c62947eb4879336c381be5d28d0ddbd4a8d
Renataandrade <34770885+Renataandrade@users.noreply.github.com>
2019-04-02 20:59:51 -0300

    Configurando Eslint e Prettier obedecendo as regras do Airbnb

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,78 +1,89 @@
-import React, { Component } from 'react';
-import { View, FlatList } from 'react-native';
-import styles from './styles';
-import { TextInput } from 'react-native-gesture-handler';
-import { connect } from 'react-redux'
-import * as allActions from '../../store/actions/todoList';
-import Todo from '../Todo';
+import React, { Component } from "react";
+import { View, FlatList } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { connect } from "react-redux";
+import * as allActions from "../../store/actions/todoList";
+import Todo from "../Todo";
+import styles from "./styles";
 
 class Search extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.todos = this.props.todos;
-      this.state = {
-        searchString: '',
-        todos: this.props.todos,
-      };
-      this.handle = this.handle.bind(this);
-    }
-    
-    regexResult = (string) => {return regex = new RegExp(string, 'gi')};
-    
-    handle = (value) => {(value !== '') ? (this.setState({ searchString: value })) : 
-        ((this.todos = this.props.todos) && (this.setState({ searchString: '' })))};
-    
-    renderItem = ({ item }) => (
-        <Todo 
-            navigation={this.props.navigation} 
-            item={item}
-            markTodo={this.props.markTodo}
-            deleteTodo={this.props.deleteTodo}
-            situation={[]} 
-        />
-    )
+  constructor(props) {
+    super(props);
 
-    render() {
-        
-      const regex = `(^|)(${this.state.searchString})(?=|)`;  
-      this.todos = (this.state.searchString !== '') ? 
-                    (this.todos.filter(a => this.regexResult(regex).test(a.title) ||
-                     this.regexResult(regex).test(a.description) ||
-                      this.regexResult(regex).test(a.date_created) || 
-                        this.regexResult(regex).test(a.date_due))) : (this.props.todos)
+    this.todos = this.props.todos;
+    this.state = {
+      searchString: "",
+      todos: this.props.todos
+    };
+    this.handle = this.handle.bind(this);
+  }
 
-      return (
-        <View style={styles.container}>
-            <View style={styles.bar}>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="Digite aqui"
-                    multiline={true}
-                    onChangeText={(e) => this.handle(e) }
-                >
-                </TextInput>
-            </View>
-            <View style={styles.listItems}>    
-                <FlatList
-                    style={styles.flat} 
-                    data={this.todos}
-                    keyExtractor={item => `${item._id}`}
-                    renderItem={this.renderItem}
-                />
-            </View>
+  regexResult = string => {
+    return (regex = new RegExp(string, "gi"));
+  };
+
+  handle = value => {
+    value !== ""
+      ? this.setState({ searchString: value })
+      : (this.todos = this.props.todos) && this.setState({ searchString: "" });
+  };
+
+  renderItem = ({ item }) => (
+    <Todo
+      navigation={this.props.navigation}
+      item={item}
+      markTodo={this.props.markTodo}
+      deleteTodo={this.props.deleteTodo}
+      situation={[]}
+    />
+  );
+
+  render() {
+    const regex = `(^|)(${this.state.searchString})(?=|)`;
+    this.todos =
+      this.state.searchString !== ""
+        ? this.todos.filter(
+            a =>
+              this.regexResult(regex).test(a.title) ||
+              this.regexResult(regex).test(a.description) ||
+              this.regexResult(regex).test(a.date_created) ||
+              this.regexResult(regex).test(a.date_due)
+          )
+        : this.props.todos;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.bar}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite aqui"
+            multiline={true}
+            onChangeText={e => this.handle(e)}
+          />
         </View>
-        );
-    }
+        <View style={styles.listItems}>
+          <FlatList
+            style={styles.flat}
+            data={this.todos}
+            keyExtractor={item => `${item._id}`}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    todos: state.todos
-})
-
-const mapDispatchToProps = dispatch => ({
-    markTodo: (_id) => dispatch(allActions.markTodo(_id)),
-    deleteTodo: (_id) => dispatch(allActions.deleteTodo(_id)),
+  todos: state.todos
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+const mapDispatchToProps = dispatch => ({
+  markTodo: _id => dispatch(allActions.markTodo(_id)),
+  deleteTodo: _id => dispatch(allActions.deleteTodo(_id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,52 +1,50 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
-import styles from'./styles';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
+import styles from "./styles";
 
 const Todo = ({ item, navigation, markTodo, deleteTodo }) => (
-    
-    <View style={styles.card}>
-        <TouchableOpacity onPress={() => markTodo(item._id)}>
-            <View>
-                <Text style={(item.pendding) ? (styles.titleMark) : (styles.title)}>
-                    {item.title}
-                </Text>
-                <Text style={styles.description}>
-                    {item.description}
-                </Text>
-            </View>
-        </TouchableOpacity>    
-        <View style={styles.cardFooter}>
-            <View style={styles.options}>
-                <Text style={styles.dates}>
-                    {`criação: ${item.date_created} \nprazo final: ${item.date_due}`}
-                </Text>
-            </View>
-            <View style={styles.options}>
-                <TouchableOpacity 
-                    onPress={() =>  navigation.navigate("PaperTodo", { item: item })} 
-                    style={styles.edit}>
-                    <EvilIcons 
-                        name='pencil' 
-                        size={30} 
-                        style={styles.editIcon} 
-                        color='purple' 
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    style={styles.delete} 
-                    onPress={() => deleteTodo(item._id)}>
-                    <EvilIcons 
-                        name='trash' 
-                        size={30} 
-                        style={styles.deleteIcon} 
-                        color='purple' 
-                    />
-                </TouchableOpacity>
-            </View>
-        </View>
+  <View style={styles.card}>
+    <TouchableOpacity onPress={() => markTodo(item._id)}>
+      <View>
+        <Text style={item.pendding ? styles.titleMark : styles.title}>
+          {item.title}
+        </Text>
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
+    <View style={styles.cardFooter}>
+      <View style={styles.options}>
+        <Text style={styles.dates}>
+          {`criação: ${item.date_created} \nprazo final: ${item.date_due}`}
+        </Text>
+      </View>
+      <View style={styles.options}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("PaperTodo", { item: item })}
+          style={styles.edit}
+        >
+          <EvilIcons
+            name="pencil"
+            size={30}
+            style={styles.editIcon}
+            color="purple"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.delete}
+          onPress={() => deleteTodo(item._id)}
+        >
+          <EvilIcons
+            name="trash"
+            size={30}
+            style={styles.deleteIcon}
+            color="purple"
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-    
+  </View>
 );
 
 export default Todo;
