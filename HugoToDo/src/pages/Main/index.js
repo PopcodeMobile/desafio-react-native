656c0a5430dbc563e4a47ee106c6573dfac207e5
Tiago Neves <tiagoneves.tia@gmail.com>
2019-05-23 10:14:23 -0300

    [DEVICE] Alterando template e emulando com sucesso no android.

--- HugoToDo/src/pages/Main/index.js
+++ HugoToDo/src/pages/Main/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HugoTODO</Text>
+    </View>
+  );
 }
