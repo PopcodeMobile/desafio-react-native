381e8cc4b4f706dd1980d942752c3ef62c69153b
Tiago Neves <tiagoneves.tia@gmail.com>
2019-05-23 09:42:15 -0300

    [HUGOTODO] Removendo arquivos indesejados e configurando o android conforme o template.

--- HugoToDo/android/app/src/main/java/com/hugotodo/MainActivity.java
+++ HugoToDo/android/app/src/main/java/com/hugotodo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.hugotodo;
 
 import com.facebook.react.ReactActivity;
+// Importações adicionadas
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +16,13 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "HugoToDo";
     }
+    @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
