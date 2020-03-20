677ef597cd32537e775725d94f702bacb1c77b41
Gabriel Menezes <gabriel@popcode.com.br>
2020-03-20 18:19:50 -0300

    Adiciona index da funcionalidade de ToDo

---
+++ app/App/Features/ToDo/index.js
@@ -0,0 +1,28 @@
+// @flow
+import ToDoScreen from './Containers/ToDoScreen'
+import type { Feature } from '../../Entities/Feature'
+
+import toDosEntityReducer from './Redux/Entity'
+import toDosUiReducer from './Redux/Ui'
+
+import successFixture from './Fixtures/getToDosSuccess'
+import failureFixture from './Fixtures/getToDosFailure'
+
+import toDosSagas from './Sagas'
+
+const feature: Feature = {
+  screens: {
+    ToDoScreen
+  },
+  sagas: toDosSagas,
+  reducers: {
+    ui: toDosUiReducer,
+    entities: toDosEntityReducer
+  },
+  fixtures: {
+    success: successFixture,
+    failure: failureFixture
+  }
+}
+
+export default feature
