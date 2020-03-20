c247c2b3a94f3c25cb4d66fbc9221add55cd24ac
Gabriel Menezes <gabriel@popcode.com.br>
2020-03-20 18:18:41 -0300

    Atualiza index geral da saga do app para adicionar as saga de Todo

--- app/App/Sagas/index.js
+++ app/App/Sagas/index.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects'
 
 import Api from '../Services/Api'
-import UserFeature from '../Features/User'
+
+import ToDoFeature from '../Features/ToDo'
 
 import Config from '../Config/DebugConfig'
 import FixtureAPI from '../Services/FixtureAPI'
@@ -9,5 +10,5 @@ import FixtureAPI from '../Services/FixtureAPI'
 const api = Config.useFixtures ? FixtureAPI : Api.create()
 
 export default function * root (): any {
-  yield all([UserFeature.sagas(api)])
+  yield all([ToDoFeature.sagas(api)])
 }
