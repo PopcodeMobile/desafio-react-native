06ea2b0b0bd53a21414069554cf1870ed2a973d4
claudio-code <suporte2424@outlook.com>
2020-04-05 05:23:40 -0300

    criado componente para mostrar ao usuario que a lista está vazia

---
+++ app/App/Features/ToDo/Components/EmptyListFromToDo/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+import styles from './styles'
+
+export default function EmptyListFromToDo () {
+  return (
+    <View style={styles.container}>
+      <Icon name='playlist-add-check' color='#000' size={42} />
+      <Text>Empty list.</Text>
+    </View>
+  )
+}
---
+++ app/App/Features/ToDo/Components/EmptyListFromToDo/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default styles
