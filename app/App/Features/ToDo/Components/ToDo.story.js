4d3609a8b7d5e0f827c9aca670c52c045d7a29e1
claudio-code <suporte2424@outlook.com>
2020-04-05 03:22:49 -0300

    alterada estrutura do codigo para facilitar o entendimento, td em uma linha nem sempre é o melhor

--- app/App/Features/ToDo/Components/ToDo.story.js
+++ app/App/Features/ToDo/Components/ToDo.story.js
@@ -4,16 +4,29 @@ import { storiesOf, addDecorator } from '@storybook/react-native'
 
 import ToDo from './ToDo'
 
-addDecorator(storyFn => <View style={styles.decorator}>{storyFn()}</View>)
+addDecorator(storyFn => <View style = { styles.decorator } > { storyFn() } </View>)
 
-storiesOf('ToDo', module)
-  .add('Default ToDo', () => <ToDo text='Title' toggled={false} onPressText={() => {}} toggleToDo={() => {}} />)
-  .add('Toggled ToDo', () => <ToDo text='Title' toggled onPressText={() => {}} toggleToDo={() => {}} />)
+    storiesOf('ToDo', module)
+    .add('Default ToDo', () => < ToDo text = 'Title'
+      toggled = { false }
+      onPressText = {
+        () => {
+          
+        } }
+      toggleToDo = {
+        () => {} }
+      />)
+      .add('Toggled ToDo', () => < ToDo text = 'Title'
+        toggled onPressText = {
+          () => {} }
+        toggleToDo = {
+          () => {} }
+        />)
 
-const styles = StyleSheet.create({
-  decorator: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+        const styles = StyleSheet.create({
+          decorator: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }
+        })
