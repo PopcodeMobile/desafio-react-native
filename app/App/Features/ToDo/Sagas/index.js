f58d3651364e60aee41f0fc909d963a0f6bffbed
claudio-code <suporte2424@outlook.com>
2020-04-08 04:01:55 -0300

    adicionado saga no saga root

--- app/App/Features/ToDo/Sagas/index.js
+++ app/App/Features/ToDo/Sagas/index.js
@@ -1,12 +1,13 @@
 // @flow
 import { all, takeLatest } from 'redux-saga/effects'
 
-import { getToDos, addNewToDo, filterToDos } from './ToDo'
+import { getToDos, addNewToDo, filterToDos, updateItemToDoIsDone } from './ToDo'
 
 export default function* root() {
   yield all([
     takeLatest('@todo/GET_ALL', getToDos),
     takeLatest('@todo/FILTER_LIST', filterToDos),
-    takeLatest('@todo/ADD_NEW', addNewToDo)
+    takeLatest('@todo/ADD_NEW', addNewToDo),
+    takeLatest('@todo/UPDATE_ITEM', updateItemToDoIsDone)
   ])
 }
