86f9279746e9f45d18c4d945035bde4a5d3077f0
claudio-code <suporte2424@outlook.com>
2020-04-08 02:38:50 -0300

    root de todos os reducers da aplicacao

--- app/App/Redux/index.js
+++ app/App/Redux/index.js
@@ -1,39 +1,8 @@
 // @flow
 import { combineReducers } from 'redux'
-import { updateReducers } from '../Config/ReduxPersist'
-import configureStore from './SetupStore'
-// import Features
-import ToDoFeature from '../Features/ToDo'
 
-// import Types
-import type { State as ToDoUIState } from '../Features/ToDo/Redux/Ui'
-import type { State as ToDoEntityState } from '../Features/ToDo/Redux/Entity'
+import todo from '../Features/ToDo/Redux/Todo'
 
-const uiReducers = combineReducers({
-  toDos: ToDoFeature.reducers?.ui
+export default combineReducers({
+  todo
 })
-
-const entityReducers = combineReducers({
-  toDos: ToDoFeature.reducers?.entities
-})
-
-const rootReducer = combineReducers({
-  ui: uiReducers,
-  entities: entityReducers
-})
-
-export default () => {
-  const store = configureStore({ rootReducer })
-  const persistor = updateReducers(store)
-
-  return { store, persistor }
-}
-
-export type GlobalState = {
-  ui: {
-    toDos: ToDoUIState
-  },
-  entities: {
-    toDos: ToDoEntityState
-  }
-}
