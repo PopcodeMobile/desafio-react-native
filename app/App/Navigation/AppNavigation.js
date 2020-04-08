653794948b682eab2b61698ed3c134babe8f04c4
claudio-code <suporte2424@outlook.com>
2020-04-08 02:37:59 -0300

    adicionado tela de add novo todo e movida a dashboard para uma tela isolada

--- app/App/Navigation/AppNavigation.js
+++ app/App/Navigation/AppNavigation.js
@@ -1,14 +1,16 @@
 // @flow
 import * as React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import ToDoScreen from '../Features/ToDo/Containers/ToDoScreen/ToDoScreen'
+
+import ToDoScreen from '../Features/ToDo/Containers/ToDoScreen'
+import AddNewToDo from '../Features/ToDo/Containers/AddNewToDo'
 
 const Stack = createStackNavigator()
-
-function AppNavigation () {
+function AppNavigation() {
   return (
     <Stack.Navigator initialRouteName='ToDo' headerMode='none'>
       <Stack.Screen name='ToDo' component={ToDoScreen} />
+      <Stack.Screen name='AddNewToDo' component={AddNewToDo} />
     </Stack.Navigator>
   )
 }
