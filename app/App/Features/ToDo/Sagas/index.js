38e9b85617dd5ba34ca91e3416e9fdcffd4ace31
claudio-code <suporte2424@outlook.com>
2020-04-08 05:15:37 -0300

    adicionado saga de update e delete no saga root

--- app/App/Features/ToDo/Sagas/index.js
+++ app/App/Features/ToDo/Sagas/index.js
@@ -1,13 +1,15 @@
 // @flow
 import { all, takeLatest } from 'redux-saga/effects'
 
-import { getToDos, addNewToDo, filterToDos, updateItemToDoIsDone } from './ToDo'
+import { getToDos, addNewToDo, filterToDos, updateItemToDoIsDone, deleteItem, updateItemToDo } from './ToDo'
 
 export default function* root() {
   yield all([
     takeLatest('@todo/GET_ALL', getToDos),
     takeLatest('@todo/FILTER_LIST', filterToDos),
     takeLatest('@todo/ADD_NEW', addNewToDo),
+    takeLatest('@todo/DELETE_ITEM_TODO', deleteItem),
+    takeLatest('@todo/UPDATE_ITEM_TODO', updateItemToDo),
     takeLatest('@todo/UPDATE_ITEM', updateItemToDoIsDone)
   ])
 }
